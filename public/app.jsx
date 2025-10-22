4ea63b41639f2c793a90dff14c7f4815aa05afcf
Животов Данило <parrotmacbookair@MacBook-Air-Zivotov.local>
2025-10-22 04:03:57 +0200

    add text

--- public/app.jsx
+++ public/app.jsx
@@ -3,7 +3,8 @@ const root = ReactDOM.createRoot(document.querySelector('.root'));
 function App() {
     return (
         <>
-            <h2>Welcome to My Bio Site</h2>
+            <h2>Welcome to My Bio Site</h2>  
+            <p>Тут после того как добавиться бд будет интересные моменты жизни</p>
         </>
     );
 }
