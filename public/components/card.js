15c7668ab973adc4b74a8305f6e5b1281aa9c00a
Danylo <parrotfromukraine@MacBook-Air-Danylo.local>
2025-10-23 23:41:02 +0200

    add component card.js

---
+++ public/components/card.js
@@ -0,0 +1,13 @@
+let db = [
+    {
+        id: '0',
+        header: 'Header',
+        descriptions: 'Description',
+        text: 'Text',
+        photolink: '../../assets/image.png',
+    },
+]
+
+console.log(db[0])
+
+export default db
