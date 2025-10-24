b3ce1ea066cd477edc0519d402ee51482e0a1082
Danylo <parrotfromukraine@MacBook-Air-Danylo.local>
2025-10-24 21:09:24 +0200

    Basic update

--- public/app.jsx
+++ public/app.jsx
@@ -3,8 +3,9 @@ const root = ReactDOM.createRoot(document.querySelector('.root'));
 function App() {
     return (
         <>
-            <h2>Welcome to My Bio Site</h2>  
-            <p>Тут после того как добавиться бд будет интересные моменты жизни</p>
+            <h2>Welcome to My Bio Site</h2> 
+            <p>Тут после того как добавиться бд будет интересные моменты жизни</p>  
+            
         </>
     );
 }
