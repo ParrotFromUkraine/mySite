4decbdf4c2bd8fd7556f5dfcc9f1f965172dad06
Danylo <parrotfromukraine@MacBook-Air-Danylo.local>
2025-12-02 16:11:02 +0100

    finish

--- public/app.jsx
+++ public/app.jsx
@@ -16,9 +16,10 @@ class constructorList {
 
 
 let whatIDo = [
+  new constructorList(4, 'Переезд', 'Переезд в Польшу в город Бяла-Подляска (Biała-Podlaska)', '30.08.2025'),
+  new constructorList(2, 'Node.js', 'learning basics framework on JavaScript', '2024-2025'),
+  new constructorList(3, 'JavaScript', 'learning basics of JavaScript', '2022-2024'),
   new constructorList(1, 'HTML/CSS', 'firsts steps in web development', '2020-2021'),
-  new constructorList(2, 'JavaScript', 'learning basics of JavaScript', '2021-2022'),
-  new constructorList(3, 'React', 'building web applications with React', '2022-2023'),
 ];
 
 console.log(whatIDo);
--- test.js
+++ test.js
@@ -11,5 +11,7 @@ let whatIDo = [
   new constructorList(1, 'HTML/CSS', 'firsts steps in web development', '2020-2021'),
   new constructorList(2, 'JavaScript', 'learning basics of JavaScript', '2021-2022'),
   new constructorList(3, 'React', 'building web applications with React', '2022-2023'),
+  new constructorList(4, 'Переезд', 'Переезд в Польшу(Biała-Podlaska)', '30.08.2025'),
 ];
 
+console.log(whatIDo);
