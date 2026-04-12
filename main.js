eb5b29ca66cde0887aea91b694117548f7994e71
ParrotFromUkraine <zivotovdanilka@gmail.com>
2026-04-13 00:45:01 +0200

    new launcher file => main.js

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
-
 const express = require('express');
-const path = require('path');
 const multer = require('multer')
-
+const os = require('os');
+const path = require('path');
+const prompt = require('prompt-sync')();
 
 const app = express();
 const port = 3000;
-const storageDisk = multer.diskStorage({
-    destination: function (req, file, cb) {
-    cb(null, 'G:/dataUp'); // <-- ТВОЙ второй диск
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + '-' + file.originalname;
-    cb(null, uniqueName);
-  }
-})
-const upload = multer({ storage: storageDisk });
 
+console.log('Операционная система:', os.type());
+
+if (prompt('Включить облако? [Y] ') === 'Y') {
+  console.log('Облако включено');
+
+  if (os.type() === 'Windows_NT') {
+    console.log('Операционная система: Windows');
+    return multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, 'D:/DataCloud'); // <-- ТВОЙ второй диск
+      },
+      filename: function (req, file, cb) {
+        const uniqueName = Date.now() + '-' + file.originalname;
+        cb(null, uniqueName);
+      }
+    });
+  }
+  
+  const upload = multer({ storage: storageDisk }); 
+
+  app.get('/up/cloud', (req, res) => {
+  res.send(`
+      <h2>Загрузка файлов</h2>
+      <form action="/main/upload/files" method="POST" enctype="multipart/form-data">
+        <input type="file" name="files" multiple />
+        <button type="submit">Отправить</button>
+      </form>
+    `)
+  });
+
+  // @route POST
+  app.post('/main/upload/files', upload.array('files', 20), (req, res) => {
+    res.json({
+      message: 'Файлы загружены',
+      files: req.files
+    });
+  });
+
+} else { 
+  console.log('Ответ не понятен, облако отключено по умолчанию');
+}
 
 // @route GET 
 app.get('/', (req, res) => {
@@ -31,23 +62,11 @@ app.get('/kbTest', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'kbTest', 'index.html'));
 });
 
-app.get('/up/cloud', (req, res) => {
- res.send(`
-    <h2>Загрузка файлов</h2>
-    <form action="/main/upload/files" method="POST" enctype="multipart/form-data">
-      <input type="file" name="files" multiple />
-      <button type="submit">Отправить</button>
-    </form>
-  `)
+app.get('/test', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'tests', 'index.html'));
 });
 
-// @route POST
-app.post('/main/upload/files', upload.array('files', 20), (req, res) => {
-  res.json({
-    message: 'Файлы загружены',
-    files: req.files
-  });
-});
+
 
 // @listener
 app.listen(port, () => {
